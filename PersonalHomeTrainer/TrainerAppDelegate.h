65a8d5afa1de881ba77067d6fc8c69cb216c8b5d
Artūrs Braučs <arturs@creo.mobi>
2012-12-28 14:34:45 +0200

    Initial Commit

---
+++ PersonalHomeTrainer/TrainerAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TrainerAppDelegate.h
+//  PersonalHomeTrainer
+//
+//  Created by Artūrs Braučs on 12/28/12.
+//  Copyright (c) 2012 Artūrs Braučs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainerAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PersonalHomeTrainer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PersonalHomeTrainer
+//
+//  Created by Artūrs Braučs on 12/28/12.
+//  Copyright (c) 2012 Artūrs Braučs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TrainerAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TrainerAppDelegate class]));
+    }
+}
